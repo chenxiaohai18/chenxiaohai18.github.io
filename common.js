79456ad317f3fb05f7f12f9c54afa8b933ea669c
Hurley Chen <471817159@qq.com>
2025-02-19 13:18:16 +0800

    Create common.js

---
+++ common.js
@@ -0,0 +1,6 @@
+function replaceAllKeyWord(str) {
+    return str.replaceAll('jiba', '鸡巴').replaceAll('guntang', '滚烫').replaceAll('yindao', '阴道').replaceAll('yinchun', '阴唇').replaceAll('taonong', '套弄').replaceAll('yuhuo', '欲火').replaceAll('mama', '妈妈').replaceAll('zuoai', '做爱').replaceAll('yinghe', '迎合').replaceAll('yinhe', '迎合')
+        .replaceAll('ying', '硬').replaceAll('ma', '吗').replaceAll('xie', '泄').replaceAll('le', '了').replaceAll('chou', '抽').replaceAll('cha', '插').replaceAll('fang', '房').replaceAll('ru', '乳').replaceAll('luan', '乱').replaceAll('lun', '伦').replaceAll('kou', '口').replaceAll('jiao', '交')
+        .replaceAll('nang', '囊').replaceAll('tao', '套').replaceAll('nong', '浓').replaceAll('mei', '妹').replaceAll('jie', '姐').replaceAll('jian', '奸').replaceAll('rou', '肉').replaceAll('bang', '棒').replaceAll('cao', '操').replaceAll('xue', '穴').replaceAll('xiao', '小').replaceAll('shuang', '爽')
+        .replaceAll('si', '死').replaceAll('zi', '子').replaceAll('gong', '宫').replaceAll('gui', '龟').replaceAll('tou', '头').replaceAll('jing', '精').replaceAll('ye', '液').replaceAll('sao', '骚').replaceAll('dong', '洞').replaceAll('yin', '淫');
+}
